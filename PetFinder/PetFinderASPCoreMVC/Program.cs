e2ffed039ad267de1f60652fa9e481279899f52a
MohammedChami <mohamuza@gmail.com>
2024-06-01 00:54:17 +0100

    added dashboard page design

--- PetFinder/PetFinderASPCoreMVC/Program.cs
+++ PetFinder/PetFinderASPCoreMVC/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Login}/{action=Index}/{id?}");
+	pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
 app.Run();
