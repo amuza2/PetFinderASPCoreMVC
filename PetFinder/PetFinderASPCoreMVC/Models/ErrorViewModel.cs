e9c66867e4abb3c57d51c1e36814960c1dbcbdc5
MohammedChami <mohamuza@gmail.com>
2024-05-31 15:14:37 +0100

    Add project files.

---
+++ PetFinder/PetFinderASPCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PetFinderASPCoreMVC.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
